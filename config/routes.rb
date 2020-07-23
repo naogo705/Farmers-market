b188ace4dfe254b470c634e144caf9d00a4081d0
naogo705 <66290412+naogo705@users.noreply.github.com>
2020-07-23 20:08:27 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'tweets#index'
-  resources :tweets
+  resources :tweets do
+    resources :comments, only: [:create]
+
+  end
   resources :users, only: :show
-  resources :comments, only: [:create]
 end
