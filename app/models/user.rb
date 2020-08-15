9a662c16133355e932b89aa32f1f34430c2e8ba7
naogo705 <66290412+naogo705@users.noreply.github.com>
2020-08-15 23:54:41 +0900

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tweets
   has_many :comments
+  has_many :favorites
+  has_many :tweets, through: :favorites
 end
