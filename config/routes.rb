a9e6cb1372a47d7a581aa047b20d4fc2adcdc3f3
naogo705 <66290412+naogo705@users.noreply.github.com>
2021-05-25 11:48:44 +0900

    hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'tweets#index'
+  root to: 'tweets#hello'
   resources :tweets do
     resources :favorites, only:[:create, :destroy]
     resources :comments, only: :create
